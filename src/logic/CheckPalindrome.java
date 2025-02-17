b648e75cbd93335ba55f5170e67d1c7835df9bf5
Ranjith Menon <ranjithmenondec@gmail.com>
2025-02-17 10:59:15 +0530

    Palindrome

---
+++ src/logic/CheckPalindrome.java
@@ -0,0 +1,22 @@
+package logic;
+
+public class CheckPalindrome {
+    public static void main(String[] args) {
+        String text = "Malayalam";
+        System.out.println(text);
+
+        String reversedText = getReversedText(text.toLowerCase());
+        if (reversedText.equals(text)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+        System.out.println(reversedText);
+
+
+    }
+
+    static String getReversedText(String text) {
+        return ReverseString.getReversedText(text);
+    }
+}
